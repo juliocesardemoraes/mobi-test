73c2f914f5fb9483486aa8a42d202d1d2ab6608c
julio cesar <jcmcf2012@gmail.com>
2024-02-04 09:54:44 -0300

    feat: extract button into a component

---
+++ src/components/UI/Button/Button.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./button.module.css";
+import { Button } from "@mui/material";
+
+export default function ButtonUI({
+  isDisabled,
+  text,
+  type,
+  onClick = () => {},
+}: {
+  isDisabled: boolean;
+  text: string;
+  type: "button" | "submit" | "reset" | undefined;
+  onClick?: any;
+}) {
+  return (
+    <Button
+      className={`mt-2 ${styles.check__button}`}
+      variant="contained"
+      disabled={isDisabled}
+      style={{ textTransform: "none" }}
+      type={type}
+      onClick={onClick}
+    >
+      {text}
+    </Button>
+  );
+}
