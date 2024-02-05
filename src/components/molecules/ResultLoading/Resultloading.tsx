080f4290c6c9c5177747d2fc94215a92a966370e
julio cesar <jcmcf2012@gmail.com>
2024-02-04 21:24:12 -0300

    test: add result skeleton testing

--- src/components/molecules/ResultLoading/Resultloading.tsx
+++ src/components/molecules/ResultLoading/Resultloading.tsx
@@ -8,18 +8,21 @@ export default function ResultLoading() {
       className={`w-100 flex flex-col ${styles.container} items-center gap-4 p-4`}
     >
       <Skeleton
+        data-testid="skeleton-1"
         className={"rounded"}
         variant="rectangular"
         width={"526px"}
         height={48}
       />
       <Skeleton
+        data-testid="skeleton-2"
         className={"rounded"}
         variant="rectangular"
         width={"150px"}
         height={48}
       />
       <Skeleton
+        data-testid="skeleton-3"
         className={"rounded"}
         variant="rectangular"
         width={"180px"}
---
+++ src/components/molecules/ResultLoading/__test__/resultloading.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import ResultLoading from "../Resultloading";
+
+describe("ResultLoading", () => {
+  it("renders Skeleton components with the specified properties", () => {
+    render(<ResultLoading />);
+
+    const skeleton1 = screen.getByTestId("skeleton-1");
+    expect(skeleton1).toHaveClass("rounded");
+    expect(skeleton1).toBeInTheDocument();
+
+    const skeleton2 = screen.getByTestId("skeleton-2");
+    expect(skeleton2).toHaveClass("rounded");
+    expect(skeleton2).toBeInTheDocument();
+
+    const skeleton3 = screen.getByTestId("skeleton-3");
+    expect(skeleton3).toHaveClass("rounded");
+    expect(skeleton3).toBeInTheDocument();
+  });
+});
